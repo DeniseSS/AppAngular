aa4e8f12a8de5a907bc916c0125ad736abb526cd
DeniseSS <https://github.com/DeniseSS>
2022-11-13 00:42:09 -0300

    inclued footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,17 +2,19 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { Error404Component } from "./component/Error 404/error404.component";
+import { FooterComponent } from "./component/footer/footer.component";
 import { MenuItensComponent } from "./component/menu/menu-component";
 
 @NgModule({
     declarations:[
         MenuItensComponent,
         Error404Component,
-        
+        FooterComponent,
     ],
     exports:[
         MenuItensComponent,
         Error404Component,
+        FooterComponent
     ],
     imports:[
         CommonModule,
